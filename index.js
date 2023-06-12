fad90988905f814f5f51ff9f2f2f0f5585b7f5d0
Aderson <1java2scrip3@gmail.com>
2023-06-12 01:14:54 -0500

    get Tasks

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const readline = require('readline');
-
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 /* Primer Modo: Lectura */
 
 let taskList = [];
@@ -16,10 +19,28 @@ function printTaskList (taskList = []) {
         }
     }
 }
-//let task = {done: null, name: null};
-addTask(taskList, 'Sacar la Basura');
+function askForTasks (taskList = []) {
+    rl.question('Introduce una Nueva Tarea (fin si terminas) ', taskName => {
+        switch (taskName) {
+            case 'fin':
+                console.log('No se Introducen ya mas tareas');
+                rl.close();
+                break;
+            case 'exit':
+                rl.close();
+                break;
+            default:
+                addTask(taskList, taskName);
+                console.log('La Lista de tareas Actual es:');
+                printTaskList(taskList);
+                askForTasks(taskList);
+        }
+    })
+}
+askForTasks(taskList);
+/* addTask(taskList, 'Sacar la Basura');
 addTask(taskList, 'Lavar los Platos');
 addTask(taskList, 'Cocinar');
 addTask(taskList, 'Programar');
-addTask(taskList, 'Jugar');
-printTaskList(taskList);
+addTask(taskList, 'Jugar'); */
+//printTaskList(taskList);
