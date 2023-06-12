c998a477d11405018e80fe6cdb29bc54971d66aa
Aderson <1java2scrip3@gmail.com>
2023-06-12 10:38:53 -0500

    Done

--- index.js
+++ index.js
@@ -10,21 +10,36 @@ function addTask (taskList = [], taskName) {
     let task = {done: false, name: taskName}
     taskList.push(task);
 }
-function printTaskList (taskList = []) {
-    for ( let task of taskList) {
-        if (task.done) {
-            console.log('[X] ' + task.name);
-        } else {
-            console.log('[] ' + task.name);
-        }
+function markTaskAsDone (taskList = [], index) {
+    if (index >= 0 && index < taskList.length) {
+        taskList[index].done = true;
+    } else {
+        console.log('Invalid Task Number');
     }
 }
-function askForTasks (taskList = []) {
+function printTaskList (taskList = []) {
+    let number = 1;
+    for ( let task of taskList) {
+        if (task.done) {
+            console.log(number + '. [X] ' + task.name);
+        } else {
+            console.log(number + '. [] ' + task.name);
+        }
+        number += 1;
+    }
+}
+function checkAllDone(taskList = []) {
+    for (let task of taskList) {
+        if (!task.done) return false;
+    }
+    return true;
+}
+function mode1 (taskList = []) {
     rl.question('Introduce una Nueva Tarea (fin si terminas) ', taskName => {
         switch (taskName) {
             case 'fin':
-                console.log('No se Introducen ya mas tareas');
-                rl.close();
+                console.log('No se Introducen ya mas tareas');               
+                mode2(taskList);
                 break;
             case 'exit':
                 rl.close();
@@ -33,11 +48,31 @@ function askForTasks (taskList = []) {
                 addTask(taskList, taskName);
                 console.log('La Lista de tareas Actual es:');
                 printTaskList(taskList);
-                askForTasks(taskList);
+                mode1(taskList);
         }
     })
 }
-askForTasks(taskList);
+/* Segundo Modo: Interaccion */
+function mode2 (taskList) {
+    printTaskList(taskList);
+    rl.question('Que tarea has realizado? (1-N) ', taskId => {
+        switch (taskId) {
+            case 'fin':
+            case 'exit':
+                console.log('Bey Bey');
+                rl.close();
+                break;
+            default:
+                markTaskAsDone(taskList, taskId - 1);
+                if (checkAllDone(taskList)) {
+                    console.log('Muy Bien Has Completado Todo...');
+                    printTaskList(taskList)
+                    rl.close();
+                } else {mode2(taskList)}
+        }
+    })
+}
+mode1(taskList);
 /* addTask(taskList, 'Sacar la Basura');
 addTask(taskList, 'Lavar los Platos');
 addTask(taskList, 'Cocinar');
